46fe968d8525c6347ddb52dba6cb938cbc493efd
SAUGER Gabriel <gabriel.sauger@loria.fr>
2025-03-13 16:19:18 +0100

    fixed var name

--- compile_no_configure.sh
+++ compile_no_configure.sh
@@ -2,7 +2,7 @@ rm apps/openssl build/bin/openssl
 mkdir -p build/bin
 make clean
 opt="$(echo $1 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
-make EXTRA_CFLAGS="$ot" -j -n > log_make.txt
+make EXTRA_CFLAGS="$opt" -j -n > log_make.txt
 if ! make EXTRA_CFLAGS="$opt" -j; then
 	echo "error make install"
 	exit 1
